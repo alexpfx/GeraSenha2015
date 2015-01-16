e127eed2fbe7807bdf6aa355efdb7587366c24d0
alexpfx <alexpfba@gmail.com>
2015-01-15 23:03:31 -0200

    initial commit

--- api-gerasenha/src/main/java/br/com/alexpfx/api/password/CharGroup.java
+++ api-gerasenha/src/main/java/br/com/alexpfx/api/password/CharGroup.java
@@ -6,7 +6,6 @@ import java.util.List;
  * Created by alexandre on 13/01/15.
  */
 public enum CharGroup {
-
     VOWELS("aeiouy"),
     CONSONANTS("bcdfghjklmnpqrstvxzw"),
     VOWELS_UPPER(VOWELS.toString().toUpperCase()),
